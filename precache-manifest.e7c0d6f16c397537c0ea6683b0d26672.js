60879615b8df807cb19efb50af322cffd2c88537
hmaranhao <herculeslima.maranhao@gmail.com>
2023-04-18 15:22:49 -0300

    Deploy React app to GitHub Pages

---
+++ precache-manifest.e7c0d6f16c397537c0ea6683b0d26672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39566d43d7d45b0e19d420f1c19ad7f9",
+    "url": "/giphy-app/index.html"
+  },
+  {
+    "revision": "ec5d2519b808f710717f",
+    "url": "/giphy-app/static/css/main.662cacc2.chunk.css"
+  },
+  {
+    "revision": "879eb6877b2ef895cc27",
+    "url": "/giphy-app/static/js/2.ba111fb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/giphy-app/static/js/2.ba111fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5d2519b808f710717f",
+    "url": "/giphy-app/static/js/main.9b2d9568.chunk.js"
+  },
+  {
+    "revision": "1d5a019864b28d94930d",
+    "url": "/giphy-app/static/js/runtime-main.07d6a809.js"
+  }
+]);
