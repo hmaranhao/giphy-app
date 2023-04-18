a8a74ca5ea3cebc09422bd1e6d28333a8e62cce8
hmaranhao <herculeslima.maranhao@gmail.com>
2023-04-18 16:10:43 -0300

    adjust api_key

--- src/server/api.js
+++ src/server/api.js
@@ -1,29 +1,32 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://api.giphy.com/v1/'
-})
+  baseURL: "https://api.giphy.com/v1/",
+});
 
-const requestSuccess = request => {
-  request.params = {...request.params, api_key: process.env.REACT_APP_GIPHY_KEY }
+const requestSuccess = (request) => {
+  request.params = {
+    ...request.params,
+    api_key: "X1doS5t2xc56iwXfvgYUusSbk5darAES",
+  };
 
-  return request
-}
+  return request;
+};
 
-const requestError = error => {
-  return Promise.reject(error)
-}
+const requestError = (error) => {
+  return Promise.reject(error);
+};
 
-const responseSuccess = response => {
-  const { data, pagination } = response.data
-  return { data, pagination }
-}
+const responseSuccess = (response) => {
+  const { data, pagination } = response.data;
+  return { data, pagination };
+};
 
-const responseError = error  => {
-  return Promise.reject(error)
-}
+const responseError = (error) => {
+  return Promise.reject(error);
+};
 
-api.interceptors.request.use(requestSuccess, requestError)
-api.interceptors.response.use(responseSuccess, responseError)
+api.interceptors.request.use(requestSuccess, requestError);
+api.interceptors.response.use(responseSuccess, responseError);
 
-export default api
+export default api;
